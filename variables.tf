216117e47385f91e87d0f759c8d0d5f796012a2f
Brian Say <brian.say91@gmail.com>
2021-06-30 13:31:34 +0100

    Add core terraform files

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  type        = string
+  description = "The IBM Cloud region where the cluster will be/has been installed."
+  default = "eu-gb"
+}
+
+variable "ibmcloud_api_key" {
+  type        = string
+  description = "The IBM Cloud api token"
+}
