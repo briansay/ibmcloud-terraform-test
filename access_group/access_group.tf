2504a6238aadb97fecd7f42d84210c411c835559
Piers Walter <piers.walter@ibm.com>
2021-07-01 12:41:34 +0100

    Added policies to admin group

--- access_group/access_group.tf
+++ access_group/access_group.tf
@@ -2,3 +2,14 @@ resource "ibm_iam_access_group" "accgrp_admin" {
   name        = "Admin_Generated"
   description = "Terraform Generated Admin Group"
 }
+
+resource "ibm_iam_access_group_policy" "services_admin_group_policy" {
+    access_group_id = ibm_iam_access_group.accgrp_admin.id
+    roles = ["Viewer","Operator","Editor", "Administrator", "Reader", "Writer", "Manager"]
+}
+
+resource "ibm_iam_access_group_policy" "account_management_admin_policy" {
+  access_group_id = ibm_iam_access_group.accgrp_admin.id
+  roles = [ "Viewer","Operator","Editor", "Administrator" ]
+  account_management = true
+}
--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 # module "access_group" {
 #   source = "./access_group"
 # }
+
+module "access_group" {
+  source = "./access_group"
+}
