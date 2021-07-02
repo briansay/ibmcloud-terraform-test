f376ea90000529b27c59b3627da798091c50cc2e
Piers Walter <piers.walter@ibm.com>
2021-07-02 13:40:01 +0100

    Added resource group to admin_access_group

--- access_group/access_group.tf
+++ access_group/access_group.tf
@@ -18,3 +18,16 @@ resource "ibm_iam_access_group_members" "accgrp_admin_members" {
   access_group_id = ibm_iam_access_group.accgrp_admin.id
   ibm_ids         = var.admin_users
 }
+
+data "ibm_resource_group" "group" {
+  name = "Default"
+}
+
+resource "ibm_iam_access_group_policy" "policy" {
+  access_group_id = ibm_iam_access_group.accgrp_admin.id
+  roles           = ["Viewer", "Operator", "Editor", "Administrator"]
+
+  resources {
+    resource_type = "resource-group"
+  }
+}
